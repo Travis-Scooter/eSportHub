8849608f50fe90df35930926981590c834e2cf22
Travis Scooter <205312476+Travis-Scooter@users.noreply.github.com>
2026-03-20 14:12:48 +0100

    CS Init

---
+++ CS_Database/Events.cs
@@ -0,0 +1,7 @@
+﻿class Events
+{
+    private DateTime[] length;
+
+    public string Host { get; set; }
+    public string Prize { get; set; }
+}
---
+++ CS_Database/Program.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+class Adatbazis : DbContext
+{
+    // táblák felsorolása
+    public DbSet<Egyik> Egyik { get; set; }
+    public DbSet<Masik> Masik { get; set; }
+
+    // kapcsolat definiálsa
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseMySQL("server=localhost;database=test;user=root");
+    }
+}
